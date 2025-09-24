256450317d71846d1dec1670b54b8b8c3841d693
Linus Fernandes <linus.fernandes@gmail.com>
2025-09-24 13:14:19 +0530

    fix: add videos.js to use while using mongosh

---
+++ chapter-4/example-3/db-fixture/videos.js
@@ -0,0 +1,13 @@
+/**
+* @class       : videos
+* @author      : Linus Fernandes (linusfernandes at gmail dot com)
+* @created     : Wednesday Sep 24, 2025 13:12:11 IST
+* @description : videos
+*/
+db.videos.insertMany([
+    {
+        "_id": ObjectId("5d9e690ad76fe06a3d7ae416"),
+        "videoPath": "SampleVideo_1280x720_1mb.mp4"
+    }
+])
+
