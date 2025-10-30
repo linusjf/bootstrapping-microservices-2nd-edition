bb3460eb5e19f73b682b1ec5e8e87fbff68fdb16
Linus Fernandes <linus.fernandes@gmail.com>
2025-10-30 19:14:48 +0530

    feat: add more tests

--- chapter-9/example-1/src/math.test.js
+++ chapter-9/example-1/src/math.test.js
@@ -1,14 +1,18 @@
-//
-// A trivial example of automated testing with Jest.
-//
-
 const { square } = require("./math");
 
 describe("square function", () => {
+  test("can square two", () => {
+    const result = square(2);
+    expect(result).toBe(4);
+  });
 
-    test("can square two", () => {
-
-        const result = square(2);
-        expect(result).toBe(4);
-    });
+  test("can square zero", () => {
+    const result = square(0);
+    expect(result).toBe(0);
+  });
+  
+  test("can square negative number", () => {
+    const result = square(-2);
+    expect(result).toBe(4);
+  });
 });
