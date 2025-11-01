572468bb8dd68aa0b6198501bd89990f4bab37dd
Linus Fernandes <linus.fernandes@gmail.com>
2025-11-01 12:49:07 +0530

    fix: update the test suite so that the database fixtures are loaded in beforeAll

--- chapter-9/example-3/src/index.test.js
+++ chapter-9/example-3/src/index.test.js
@@ -6,86 +6,82 @@ const axios = require("axios");
 const mongodb = require("mongodb");
 
 describe("metadata microservice", () => {
-    
-    const DBHOST = "mongodb://localhost:27017"; // Have the database running on this computer.
-    const DBNAME = "testdb";
-    const PORT = 3000;
-    const BASE_URL = `http://localhost:${PORT}`; // Base URL for our HTTP server.
+  const DBHOST = "mongodb://localhost:27017"; // Have the database running on this computer.
+  const DBNAME = "testdb";
+  const PORT = 3000;
+  const BASE_URL = `http://localhost:${PORT}`; // Base URL for our HTTP server.
 
-    //
-    // Import the module we are testing.
-    //
+  //
+  // Import the module we are testing.
+  //
 
-    const { startMicroservice } = require("./index"); 
+  const { startMicroservice } = require("./index");
 
-    //
-    // Setup the HTTP server.
-    //
+  //
+  // Setup the HTTP server.
+  //
 
-    let microservice; // Saves a reference to the our microservice object.
+  let microservice; // Saves a reference to the our microservice object.
+  const id1 = new mongodb.ObjectId();
+  const id2 = new mongodb.ObjectId();
+  const videoPath1 = "my-video-1.mp4";
+  const videoPath2 = "my-video-2.mp4";
 
-    beforeAll(async () => {
-        microservice = await startMicroservice(DBHOST, DBNAME, PORT); // Start server before all tests.
-    });
+  const testVideos = [
+    {
+      _id: id1,
+      videoPath: videoPath1,
+    },
+    {
+      _id: id2,
+      videoPath: videoPath2,
+    },
+  ];
 
-    afterAll(async () => {
-        await microservice.close();  // Close server after all tests.
-    });
+  beforeAll(async () => {
+    microservice = await startMicroservice(DBHOST, DBNAME, PORT); // Start server before all tests.
 
-    //
-    // Wrapper function for doing a HTTP GET request so that we don't have to repeat the base URL 
-    // across multiple tests.
-    //
-    function httpGet(route) {
-        const url = `${BASE_URL}${route}`;
-        console.log(`Requesting ${url}`);
-        return axios.get(url);
-    }
+    // Load database fixture into the database.
+    await loadDatabaseFixture("videos", testVideos);
+  });
 
-    // 
-    // Helper function to load a database fixture into our database.
-    //
-    async function loadDatabaseFixture(collectionName, records) {
-        await microservice.db.dropDatabase(); // Reset the test database.
+  afterAll(async () => {
+    await microservice.close(); // Close server after all tests.
+  });
 
-        const collection = microservice.db.collection(collectionName);
-        await collection.insertMany(records); // Insert the database fixture.
-    }
-    
-    //
-    // Tests go here.
-    //
-    
-    test("/videos route retrieves data via videos collection", async () => {
+  //
+  // Wrapper function for doing a HTTP GET request so that we don't have to repeat the base URL
+  // across multiple tests.
+  //
+  function httpGet(route) {
+    const url = `${BASE_URL}${route}`;
+    console.log(`Requesting ${url}`);
+    return axios.get(url);
+  }
 
-        const id1 = new mongodb.ObjectId();
-        const id2 = new mongodb.ObjectId();
-        const videoPath1 = "my-video-1.mp4";
-        const videoPath2 = "my-video-2.mp4";
+  //
+  // Helper function to load a database fixture into our database.
+  //
+  async function loadDatabaseFixture(collectionName, records) {
+    await microservice.db.dropDatabase(); // Reset the test database.
 
-        const testVideos = [
-            {
-                _id: id1,
-                videoPath: videoPath1
-            },
-            {
-                _id: id2,
-                videoPath: videoPath2
-            },
-        ];
+    const collection = microservice.db.collection(collectionName);
+    await collection.insertMany(records); // Insert the database fixture.
+  }
 
-        // Load database fixture into the database.
-        await loadDatabaseFixture("videos", testVideos);
-        
-        const response = await httpGet("/videos");  // Make a request to the videos route.
-        expect(response.status).toEqual(200);       // Expect HTTP status code 200 (ok).
+  //
+  // Tests go here.
+  //
 
-        const videos = response.data.videos;        // Check the videos retrieved are the ones we put in the database.
-        expect(videos.length).toEqual(2);
-        expect(videos[0]._id).toEqual(id1.toString());
-        expect(videos[0].videoPath).toEqual(videoPath1);
-        expect(videos[1]._id).toEqual(id2.toString());
-        expect(videos[1].videoPath).toEqual(videoPath2);
-    });
+  test("/videos route retrieves data via videos collection", async () => {
+    const response = await httpGet("/videos"); // Make a request to the videos route.
+    expect(response.status).toEqual(200); // Expect HTTP status code 200 (ok).
 
+    const videos = response.data.videos; // Check the videos retrieved are the ones we put in the database.
+    expect(videos.length).toEqual(2);
+    expect(videos[0]._id).toEqual(id1.toString());
+    expect(videos[0].videoPath).toEqual(videoPath1);
+    expect(videos[1]._id).toEqual(id2.toString());
+    expect(videos[1].videoPath).toEqual(videoPath2);
+  });
 });
